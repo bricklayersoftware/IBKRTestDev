7b36df7676c2b1f6a70723504892f3fe2e897b51
bricklayersoftware <aVlCM@outlook.com>
2025-09-14 08:13:18 -0400

    modified

--- IBKRPython/tcpipclient.py
+++ IBKRPython/tcpipclient.py
@@ -1,4 +1,12 @@
 import socket
+from datetime import datetime
+import time
+
+def timestamp():
+    # Format the datetime object into a string
+    now = datetime.now()
+    ts = now.strftime("%Y-%m-%d %H:%M:%S")
+    return ts
 
 # Define the server's IP address and port
 SERVER_IP = '52.188.185.179'  # Use 'localhost' or the actual IP of your server
@@ -14,16 +22,16 @@ try:
 
     while True:
         # Send data to the server
-        message = "type hello"
+        message = "client says hello: "+timestamp()
         client_socket.sendall(message.encode('utf-8'))
         print(f"Sent: {message}")
+        
+        time.sleep(1)
 
         # Receive data from the server
         data = client_socket.recv(1024)  # Receive up to 1024 bytes
         print(f"Received: {data.decode('utf-8')}")
 
-except ConnectionRefusedError:
-    print(f"Connection refused. Ensure the server is running and listening on {SERVER_IP}:{SERVER_PORT}")
 except Exception as e:
     print(f"An error occurred: {e}")
 finally:
