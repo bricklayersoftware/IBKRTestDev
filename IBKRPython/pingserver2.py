4a1eff289940c774f16ed5487a9419dcd356a998
Sebastian Wolf <sebastianwolf2947@proton.me>
2025-09-14 08:11:46 -0400

    modified

--- IBKRPython/pingserver2.py
+++ IBKRPython/pingserver2.py
@@ -11,13 +11,14 @@ logging.debug("This is a debug message.")
 logging.info("This is an informational message.")
 
 # Configuration
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+HOST = '0.0.0.0'  # 52.188.185.179
 PORT = 44444        # Port to listen on (non-privileged ports are > 1023)
 
 conn = None
 client_addr = None
 client_ip = None
 client_port = None
+server_socket = None
 
 shared_data = []  # Data to be sent/received
 data_lock = threading.Lock() # Lock to protect shared_data
@@ -29,18 +30,25 @@ def timestamp():
     return ts
 
 def receive_thread():
-
+    global conn
+    
     logging.info(f"receive_thread: "+timestamp())
 
     while True:
+        if conn == None:
+            logging.info("receive_thread: no connection, nothing to receive from")
+            time.sleep(1)
+            continue
+        
         try:
-            data = conn.recv(1024)
+            data = conn.recv(1024) # blocks until something received (sent by client)
+            
             if not data:
                 break
             
             message = data.decode('utf-8')
 
-            response = f"received bytes: {len(message)}"
+            response = f"received message: [{message}] {len(data)} bytes " +timestamp()
 
             logging.info(response)
 
@@ -48,15 +56,15 @@ def receive_thread():
             with data_lock:
                 shared_data.append(message)
             
-            # Send a response back to the client
-            conn.sendall(response.encode('utf-8')) # text string: response.encode('utf-8'))
-
         except Exception as e:
-            logging.info(f"Error handling client: {e}")
-            break
-
-    logging.info(f"Client disconnected.")
-    conn.close()
+            logging.info(f"receive_thread: exception: {e}")
+            conn = None
+            continue
+        
+    if conn:
+        conn.close()
+        
+    conn = None
 
 def send_thread():
     global conn
@@ -66,40 +74,68 @@ def send_thread():
     while True:
         time.sleep(1)  
 
-        message_to_send = "hello, word! "+timestamp()
+        message_to_send = "server says hello, word! "+timestamp()
 
-        if message_to_send:
-            print(f"Sending to all clients: {message_to_send}")
-            try:
-                conn.sendall(message_to_send.encode('utf-8'))
-            except Exception as e:
-                logging.info(f"Error sending to a client: {e}")
+        if conn == None:
+            logging.info("send_thread: connection is none")
+            continue
+        
+        logging.info(f"sending to client: [{message_to_send}]")
+        try:
+            conn.sendall(message_to_send.encode('utf-8'))
+        except Exception as e:
+            logging.info(f"Error sending to a client: {e}")
+            conn = None
 
 
+def accept_conn_thread():
+    global server_socket
+    global conn
+    global client_addr
+    global client_ip
+    global client_port
+    
+    while True:    
+
+        if conn == None:
+            server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server_socket.bind((HOST, PORT))
+            server_socket.listen()
+
+            logging.info(f"listening on {HOST}:{PORT}")
+            logging.info("waiting for new connection...")
+            conn, client_addr = server_socket.accept() # wait for client to connect
+            client_ip, client_port = client_addr
+
+            logging.info(f"client connected with IP:port {client_ip}:{client_port}")
+
+        else:
+            time.sleep(1)
+            logging.info("connection active")
+            
+    
 def main():
     global conn
     global client_ip
     global client_port
     global client_addr
+    global server_socket
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((HOST, PORT))
-    server_socket.listen()
-
-    logging.info(f"listening on {HOST}:{PORT}")
-    
-    sender_thread = threading.Thread(target=send_thread)
-    sender_thread.daemon = True # Allows the main program to exit even if this thread is running
-    sender_thread.start()
-
-    conn, client_addr = server_socket.accept() # wait for client to connect
-    client_ip, client_port = client_addr
-
-    logging.info(f"client IP:port {client_ip}:{client_port}")
+    conn_thread = threading.Thread(target=accept_conn_thread)
+    conn_thread.daemon = True
+    conn_thread.start()
 
     client_thread = threading.Thread(target=receive_thread)
     client_thread.daemon = True
     client_thread.start()
 
+    sender_thread = threading.Thread(target=send_thread)
+    sender_thread.daemon = True
+    sender_thread.start()
+
+    while True:
+        logging.info("pulse "+timestamp())
+        time.sleep(1)
+        
 if __name__ == "__main__":
     main()
